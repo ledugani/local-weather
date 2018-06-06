a1ea9c39cba49bbaf1fee9d4b75c2087513212cf
Tom Dugan <thomas@aadwebb.com>
2018-06-05 21:23:28 -0500

    fully functional delete

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,18 +1,20 @@
 // One Day
 
 const oneDayDomString = (weatherArray) => {
+  console.log(weatherArray);
   let strang = '';
-  strang += `<div class="col-sm-6 col-md-4 col-md-offset-4">`;
+  strang += `<div  id="${weatherArray.dt}" class="day col-sm-6 col-md-4 col-md-offset-4">`;
   strang +=  `<div class="thumbnail forecast">`;
+  strang +=   `<div class="panel-title hide">Today</div>`;
   strang +=   `<div class="caption">`;
   strang +=     `<h3>${weatherArray.name}</h3>`;
-  strang +=     `<img src="https://openweathermap.org/img/w/${weatherArray.weather[0].icon}.png">`;
-  strang +=     `<h4>${Math.ceil(weatherArray.main.temp)}&#8457;, ${weatherArray.weather[0].main}</h4>`;
-  strang +=     `<p><strong>Current Conditions: </strong>${weatherArray.weather[0].description}</p>`;
-  strang +=     `<p><strong>Barometric Pressure: </strong>${weatherArray.main.pressure} hPa</p>`;
-  strang +=     `<p><strong>Wind Speed: </strong>${weatherArray.wind.speed} mph</p>`;
-  strang +=     `<p><strong>Humidity: </strong>${weatherArray.main.humidity}%</p>`;
-  strang +=     `<p><button type="button" id="${weatherArray.dt}" class="btn btn-success save-forecast">Save Forecast</button>
+  strang +=     `<img class="icon" src="https://openweathermap.org/img/w/${weatherArray.weather[0].icon}.png">`;
+  strang +=     `<h4><p class="temperature">${Math.ceil(weatherArray.main.temp)}</p>&#8457;, ${weatherArray.weather[0].main}</h4>`;
+  strang +=     `<p><strong>Current Conditions: </strong><item class="conditions">${weatherArray.weather[0].description}</item></p>`;
+  strang +=     `<p><strong>Barometric Pressure: </strong><item class="pressure">${weatherArray.main.pressure}</item> hPa</p>`;
+  strang +=     `<p><strong>Wind Speed: </strong><item class="windspeed">${weatherArray.wind.speed}</item> mph</p>`;
+  strang +=     `<p><strong>Humidity: </strong><item class="humidity">${weatherArray.main.humidity}</item>%</p>`;
+  strang +=     `<p><button type="button" class="btn btn-success save-forecast">Save Forecast</button>
   <a href="#" id="fiveday" class="btn btn-default" role="button">5-Day</a></p>`;
   strang +=   `</div>`;
   strang +=  `</div>`;
@@ -30,10 +32,10 @@ const fiveDayDomString = (weatherArray) => {
   days.forEach((day, index) => {
     if (index % 8 === 3) {
       if (counter === 0) {
-        newStrang += `<div id="${day.dt}" class="forecast panel panel-info col-md-2 col-md-offset-1 day no-padding">`;
+        newStrang += `<div id="${day.dt} no-padding" class="forecast panel panel-info col-md-2 col-md-offset-1 day">`;
         counter++;
       } else {
-        newStrang += `<div id="${day.dt}" class="forecast panel panel-info col-md-2 day no-padding">`;
+        newStrang += `<div id="${day.dt} no-padding" class="forecast panel panel-info col-md-2 day">`;
       };
       newStrang +=  `<div class="panel-heading">`;
       newStrang +=    `<h4 class="panel-title">${day.dt_txt}</h4>`;
@@ -69,7 +71,7 @@ const savedDomString = (savedForecast) => {
     latestDomString +=  `</div>`;
     latestDomString +=  `<div class="panel-body">`;
     latestDomString +=    `<img class="icon" src="${forecast.icon}">`;
-    latestDomString +=    `<h4>${forecast.temp}</h4>`;
+    latestDomString +=    `<h4>${forecast.temp}&#8457;</h4>`;
     latestDomString +=    `<h6><strong>Conditions:</strong> ${forecast.description}</h6>`;
     latestDomString +=    `<h6><strong>Barometric Pressure:</strong> ${forecast.pressure}hPa</h6>`;
     latestDomString +=    `<h6><strong>Wind Speed:</strong> ${forecast.speed}mph</h6>`;
